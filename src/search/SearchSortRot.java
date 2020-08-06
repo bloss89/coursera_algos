d8342865eff6418f9d8bde712bcdf8fdb8d1f7a0
bloss89 <bawesome357@gmail.com>
2020-08-07 03:28:49 +0530

    search in sorted n rotated array

---
+++ src/search/SearchSortRot.java
@@ -0,0 +1,26 @@
+package search;
+
+public class SearchSortRot {
+
+	public static int search(int arr[], int l, int h, int key) {
+		if (l > h)
+			return -1;
+		int mid = (l + h) / 2;
+		if (arr[mid] == key)
+			return mid;
+		if (arr[l] <= arr[mid]) {
+			if (key >= arr[l] && key <= arr[mid])
+				return search(arr, l, mid - 1, key);
+			return search(arr, mid + 1, h, key);
+		}
+		if (key >= arr[mid] && key <= arr[h])
+			return search(arr, mid + 1, h, key);
+
+		return search(arr, l, mid - 1, key);
+	}
+
+	public static void main(String args[]) {
+		int arr[] = { 4, 5, 6, 7, 8, 9, 1, 2, 3 };
+		System.out.println(search(arr, 0, arr.length - 1, 6));
+	}
+}
